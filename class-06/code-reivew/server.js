58beaec5f2e993a03d2fbf8f32661c221e33eb56
LENA EIVY <applena@gmail.com>
2020-02-20 17:23:18 -0800

    day 9 updated

--- class-06/code-reivew/server.js
+++ class-06/code-reivew/server.js
@@ -1,30 +1,64 @@
 'use strict';
 
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const superagent = require('superagent');
+const pg = require('pg');
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', err => console.error(err));
 
 const cors = require('cors');
 app.use(cors());
 
-require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/location', (request, response) => {
 
-    let city = request.query.city;
-    // let geoData = require('./data/geo.json');
-    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API}&q=${city}&format=json`;
+  let city = request.query.city;
+  // look in my DB to see if location already exists
 
-    superagent.get(url)
-      .then(results => {
-        console.log('results from superagent', results.body);
-        let geoData = results.body;
-        let location = new City(city, geoData[0]);
-        response.status(200).send(location);
-      })
+  let sql = "SELECT * FROM locations WHERE search_query=$1;";
+  let safeValues = [city];
 
+  client.query(sql, safeValues)
+    .then(results => {
+      if(results.rows.length > 0){
+        console.log('found the city in the DB')
+        // if it does, send that file to the front end
+          // it will make my life easier if the structure of the DB data is in the same format as what the front end is expecting (aka - my table should have the same keys as my constructor)
+        response.send(results.rows[0]);
+        // results.rows = [{
+          // search_query:'seattle',
+          // formatted_query:'Seattle, WA',
+          // latitude: '23piqt;jrgo',
+          // longitude: 'a;ekrjgapir'
+        //}]
+      } else {
+        console.log('did not find the city in the DB. Going to the API')
+        // if it doesn't, then I need to go to the API and get the data
+          // save it to the database
+          // send it to the frontend
+          // let geoData = require('./data/geo.json');
+          let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API}&q=${city}&format=json`;
+      
+          superagent.get(url)
+            .then(results => {
+              console.log('results from superagent', results.body);
+              let geoData = results.body;
+              let location = new City(city, geoData[0]);
+
+              let sql="INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);";
+              let safeValues = [location.search_query, location.formatted_query, location.latitude, location.longitude];
+
+              client.query(sql, safeValues);
+
+              response.status(200).send(location);
+            }).catch(console.error(err));
+      }
+    })
 })
 
 app.get('/weather', (request, response) => {
@@ -58,6 +92,9 @@ function Weather(day){
   this.forecast = day.summary;
 }
 
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-})
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`);
+    })
+  })
