671920322789f5f22d835d4e95ba46c9307a036b
LENA EIVY <applena@gmail.com>
2020-02-21 16:44:41 -0800

    day 10

--- class-06/code-reivew/server.js
+++ class-06/code-reivew/server.js
@@ -15,7 +15,59 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/location', (request, response) => {
+app.get('/location', handleLocation);
+app.get('/weather', handleWeather);
+app.get('/yelp', handleYelp);
+
+function handleYelp(request, response){
+  let city = request.query.search_query;
+  // { search_query: 'austin',
+  // formatted_query: 'Austin, Travis County, Texas, USA',
+  // latitude: '30.2711286',
+  // longitude: '-97.7436995' }
+
+  let url = `https://api.yelp.com/v3/businesses/search?location=${city}`;
+
+  superagent.get(url)
+    .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+    .then(results => {
+      console.log('😎',results.body.businesses);
+      let dataObj = results.body.businesses.map(business => {
+        return new Yelp(business);
+      })
+      response.status(200).send(dataObj);
+  })
+}
+
+
+function Yelp(obj){
+  this.name = obj.name;
+  this.image_url = obj.image_url;
+  this.price = obj.price;
+  this.rating = obj.rating;
+  this.url = obj.url;
+}
+
+function handleWeather(request, response){
+    let {search_query, formatted_query, latitude, longitude} = request.query;
+    // { search_query: 'tacoma',
+    // formatted_query: 'Lynnwood, Snohomish County, Washington, USA',
+    // latitude: '47.8278656',
+    // longitude: '-122.3053932' }
+  
+    let darkSky = require('./data/darksky.json');
+  
+    let weatherArray = darkSky.daily.data;
+  
+    let newWeatherArray = [];
+    weatherArray.forEach(day => {
+      newWeatherArray.push(new Weather(day));
+    })
+  
+    response.send(newWeatherArray)
+}
+
+function handleLocation(request, response){
 
   let city = request.query.city;
   // look in my DB to see if location already exists
@@ -46,7 +98,7 @@ app.get('/location', (request, response) => {
       
           superagent.get(url)
             .then(results => {
-              console.log('results from superagent', results.body);
+              // console.log('results from superagent', results.body);
               let geoData = results.body;
               let location = new City(city, geoData[0]);
 
@@ -59,26 +111,8 @@ app.get('/location', (request, response) => {
             }).catch(console.error(err));
       }
     })
-})
+}
 
-app.get('/weather', (request, response) => {
-  let {search_query, formatted_query, latitude, longitude} = request.query;
-  // { search_query: 'tacoma',
-  // formatted_query: 'Lynnwood, Snohomish County, Washington, USA',
-  // latitude: '47.8278656',
-  // longitude: '-122.3053932' }
-
-  let darkSky = require('./data/darksky.json');
-
-  let weatherArray = darkSky.daily.data;
-
-  let newWeatherArray = [];
-  weatherArray.forEach(day => {
-    newWeatherArray.push(new Weather(day));
-  })
-
-  response.send(newWeatherArray)
-})
 
 function City(city, obj){
   this.search_query = city;
@@ -88,9 +122,11 @@ function City(city, obj){
 }
 
 function Weather(day){
-  this.time = new Date(day.time).toDateString();
+  this.time = new Date(day.time*1000).toDateString();
   this.forecast = day.summary;
 }
+
+
 
 client.connect()
   .then(() => {
